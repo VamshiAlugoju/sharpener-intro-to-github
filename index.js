1e789902d357234050244618725d3cb2fb78d9f2
vamshi <alugojuvamshi@gmail.com>
2023-04-03 14:07:12 +0530

    updated

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
+ let form = document.querySelector("#addForm");
+ let ul = document.querySelector("#items");
+ form.addEventListener("submit",addelement);
+
+//  console.log(ul)
+
+ function addelement(e)
+ {    
+    e.preventDefault();
+
+    let name = document.querySelector("#addForm #item");
+    let li = document.createElement("li");
+    li.innerText  =  name.value;
+    li.className = "list-group-item d-flex justify-content-between";
+
+    let btn = document.createElement("button");
+    btn.className = "btn btn-danger btn-sm float-right delete"
+    btn.innerText = "X"
+    li.appendChild(btn);
+    ul.appendChild(li);
+ }
+
+ let deletebtn = document.querySelectorAll("#items .list-group-item .delete");
+ for(let btn of deletebtn)
+ {
+     btn.addEventListener("click" , remove);
+ }
  
- let title = document.querySelector(".title");
- title.style.color = "green";
- title.style.font = "bold";
- title.style.border = "3px solid black"
-
- // one
-
-
- let list = document.getElementsByClassName("list-group-item");
- list[2].style.backgroundColor = "green"
-  
-  for(l of list)
-  {
-    l.style.fontWeight = "bold"
-  }
-  
-  let item = document.querySelectorAll(".list-group")[0].lastElementChild;
-
-  item.classList.add("list-group-item"); 
-
-// two
-
-  let liTags = document.getElementsByTagName("li");
-  let n = liTags.length-1;
-  liTags[n].style.fontWeight = "bold"
-
-  let secondItem = document.querySelector(".list-group-item:nth-child(2)");
-  console.log(secondItem);
-  secondItem.style.backgroundColor = "green";
-  let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
-  thirdItem.style.display = "none"
-
-  // three
-
-  let container = document.querySelector(".container");
-  let h3 = document.createElement("h3");
-  h3.innerText = "hello World";
-  container.appendChild(h3);
-
-  let ulitems = document.querySelector("#items");
-  let listelement  = document.createElement("li");
-  listelement.classList.add("list-group-item");
-  listelement.innerText = "hello world";
-  ulitems.insertBefore(listelement,ulitems.firstChild);
-
-  //four
-  
+ function remove(e)
+ {   
+     let btn = e.target;
+     console.log()
+     let liitem = btn.parentElement.parentElement;
+     ul.removeChild(liitem)
+     
+ }
