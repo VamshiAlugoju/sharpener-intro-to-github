7f785a5c13262ddff4d841628c76769810226dc0
vamshi <alugojuvamshi@gmail.com>
2023-04-03 13:28:30 +0530

    updated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  title.style.font = "bold";
  title.style.border = "3px solid black"
 
+ // one
+
 
  let list = document.getElementsByClassName("list-group-item");
  list[2].style.backgroundColor = "green"
@@ -17,6 +19,8 @@
 
   item.classList.add("list-group-item"); 
 
+// two
+
   let liTags = document.getElementsByTagName("li");
   let n = liTags.length-1;
   liTags[n].style.fontWeight = "bold"
@@ -26,3 +30,19 @@
   secondItem.style.backgroundColor = "green";
   let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
   thirdItem.style.display = "none"
+
+  // three
+
+  let container = document.querySelector(".container");
+  let h3 = document.createElement("h3");
+  h3.innerText = "hello World";
+  container.appendChild(h3);
+
+  let ulitems = document.querySelector("#items");
+  let listelement  = document.createElement("li");
+  listelement.classList.add("list-group-item");
+  listelement.innerText = "hello world";
+  ulitems.insertBefore(listelement,ulitems.firstChild);
+
+  //four
+  
