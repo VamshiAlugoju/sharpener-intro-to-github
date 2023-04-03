59c7b563a5e04e03ad8faa205de1bcde55ee0bf9
vamshi <alugojuvamshi@gmail.com>
2023-04-03 12:01:20 +0530

    updated

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
  title.style.color = "green";
  title.style.font = "bold";
  title.style.border = "3px solid black"
+
+
+ let list = document.getElementsByClassName("list-group-item");
+ list[2].style.backgroundColor = "green"
+  
+  for(l of list)
+  {
+    l.style.fontWeight = "bold"
+  }
