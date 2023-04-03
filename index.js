db81731a9b9740a105ef8385b2f1188b85c3c1be
vamshi <alugojuvamshi@gmail.com>
2023-04-03 15:08:42 +0530

    updated

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
  let form = document.querySelector("#addForm");
  let ul = document.querySelector("#items");
- form.addEventListener("submit",addelement);
+ let search = document.querySelector("#filter");
 
-//  console.log(ul)
+ form.addEventListener("submit",addelement);
+ search.addEventListener("keyup",filter)
+//  console.log(search)
 
  function addelement(e)
  {    
     e.preventDefault();
 
     let name = document.querySelector("#addForm #item");
+    let description = document.querySelector("#addForm #description")
+
     let li = document.createElement("li");
-    li.innerText  =  name.value;
+    let div = document.createElement("div");
+    li.innerText  =  name.value + " " + description.value;
     li.className = "list-group-item d-flex justify-content-between";
 
     let btn = document.createElement("button");
-    btn.className = "btn btn-danger btn-sm float-right delete"
+    let editbtn = document.createElement("button");
+    btn.className = "btn btn-danger  btn-sm delete"
+    editbtn.className = "btn btn-primary btn-sm "
     btn.innerText = "X"
-    li.appendChild(btn);
+    editbtn.innerText = "edit"
+    div.appendChild(btn);
+    div.appendChild(editbtn);
+      
+    li.appendChild(div)
     ul.appendChild(li);
  }
 
@@ -32,5 +43,29 @@
      console.log()
      let liitem = btn.parentElement.parentElement;
      ul.removeChild(liitem)
-     
+ }
+
+ function filter(e)
+ {
+    let text = e.target.value.toLowerCase();
+    // console.log(text)
+    let items = ul.querySelectorAll("li");
+
+    // console.log(items)
+    Array.from(items).forEach((item)=>{
+          
+        let itemName = item.firstChild.textContent.toLowerCase();
+        if(itemName.indexOf(text) != -1)
+        {
+            item.style.display ="block";
+            item.classList.add("d-flex")
+            // console.log(itemName)
+        }
+        else{
+            item.style.display ="none"
+            item.classList.remove("d-flex")
+          
+        }
+    })
+
  }
