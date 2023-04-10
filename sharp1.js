c12c08cd67ef96e3ccc4482ee83e677db6932252
vamshi <alugojuvamshi@gmail.com>
2023-04-10 18:26:29 +0530

    axios

--- sharp1.js
+++ sharp1.js
@@ -1,1 +1,21 @@
-console.log("ha ha ha");
+function dothis(e) {
+  e.preventDefault();
+  let user = e.target;
+  // console.log(user.name.value,user.email.value,user.phone.value)
+  let obj = {
+    name: user.name.value,
+    email: user.email.value,
+    phone: user.phone.value,
+  };
+  axios
+    .post(
+      "https://crudcrud.com/api/5e23e7d240c14587b1b0194169f1a010/users",
+      obj
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+}
